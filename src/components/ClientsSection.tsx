c91f8740768cddd4b5fdd298159a126440ab565c
unknown <ricopozzi@hotmail.com>
2022-06-15 19:53:30 -0300

    instagram browser fix

--- src/components/ClientsSection.tsx
+++ src/components/ClientsSection.tsx
@@ -1,14 +1,18 @@
 export function ClientsSection() {
   return (
     <>
-      <main className='h-[20vh] pt-4 mt-14 flex flex-col items-center'>
+      <main className='min-h-[20vh] pt-4 mt-14 flex flex-col items-center'>
         <h1 className='text-center text-xl font-bold mb-8 '>Clientes:</h1>
-        <div className='flex gap-6 md:gap-10'>
-          <img src='netflix.svg' className='w-28 md:w-32' />
+        <div className='flex gap-x-[3%] gap-y-3 md:gap-10 flex-wrap justify-center'>
+          <img src='netflix.svg' className='w-[30%] md:w-32' />
 
-          <img src='pilao.webp' alt='' className='w-28 md:w-32 h-20' />
+          <img src='pilao.webp' alt='' className='w-[30%] md:w-32 h-20' />
 
-          <img src='caras.svg' alt='revista caras' className='w-28 md:w-32' />
+          <img
+            src='caras.svg'
+            alt='revista caras'
+            className='w-[30%] md:w-32'
+          />
         </div>
       </main>
     </>
